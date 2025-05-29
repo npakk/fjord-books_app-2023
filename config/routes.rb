78d379dc1efb02fe02c6a2e2cbb89f61fd251032
npakk <npakk669@gmail.com>
2025-05-29 11:06:39 +0900

    ログイン画面をrootにする

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users
+
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
+
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
