e260c2c3d9f8e52282adf9af6ea2c8db0857fec9
npakk <npakk669@gmail.com>
2025-05-31 01:57:39 +0900

    UserのViewだけ別レイアウトを適用
    ログイン画面やサインアップ画面でDevise側のログインボタンとは別に、
    上部ナビゲーションのログインボタンが表示されていた問題を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout
 
   protected
 
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     books_path
+  end
+  
+  def layout
+    if devise_controller?
+      "user_layout"
+    else
+      "application"
+    end
   end
 end
