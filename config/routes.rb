01d3c3cca4e8d3cc45f6fa8afa6b21a2d9676d26
npakk <npakk669@gmail.com>
2025-06-01 13:02:34 +0900

    ユーザー一覧画面と詳細画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :books
+  resources :users, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
