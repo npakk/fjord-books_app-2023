bb121daa112b89e8c3d8438ea235da08446c73a1
npakk <npakk669@gmail.com>
2025-06-01 13:15:19 +0900

    住所などが登録されていなかった問題を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username postcode address biography])
   end
 
   def after_sign_in_path_for(resource)
