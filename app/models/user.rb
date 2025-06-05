89e9efc3725aad15c266d1f9c410e9959dac4f3f
npakk <npakk669@gmail.com>
2025-06-05 22:49:17 +0900

    rubocop/erblint対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :icon
   validate :image_content_type
-  
+
   def image_content_type
-    prefix = 'image/'
-    extensions = %w(jpeg jpg gif png)
-    errors.add(:icon, :icon_extension, extensions: extensions.map(&:upcase).join('・')) unless icon.content_type.in?(extensions.map{|e| 'image/' + e})
+    extensions = %w[jpeg jpg gif png]
+    errors.add(:icon, :icon_extension, extensions: extensions.map(&:upcase).join('・')) unless icon.content_type.in?(extensions.map { |e| "image/#{e}" })
   end
 end
