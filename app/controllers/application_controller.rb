5d40c5a3afd172d0f68ac3f6276b7fc9d3513856
npakk <npakk669@gmail.com>
2025-06-06 13:13:45 +0900

    permitメソッドに渡すkeysをDRYに書く

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username postcode address biography])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[username postcode address biography])
+    keys = %i[username postcode address biography]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 
   def after_sign_in_path_for(_resource)
