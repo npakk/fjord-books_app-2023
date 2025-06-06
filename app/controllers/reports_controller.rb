bd8a14dd7bd73449f4e3169581560bfbe7febe81
npakk <npakk669@gmail.com>
2025-06-07 01:12:12 +0900

    Reportのuser_idに投稿したユーザーのidを登録する

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,6 +22,7 @@ class ReportsController < ApplicationController
   # POST /reports or /reports.json
   def create
     @report = Report.new(report_params)
+    @report.user_id = current_user.id
 
     respond_to do |format|
       if @report.save
@@ -65,6 +66,6 @@ class ReportsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def report_params
-      params.require(:report).permit(:user_id, :title, :body)
+      params.require(:report).permit(:title, :body)
     end
 end
