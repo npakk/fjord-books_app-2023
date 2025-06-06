f33ae35d4ad39065e1ba2e92ad0859e8d505afde
npakk <npakk669@gmail.com>
2025-06-06 16:07:35 +0900

    rubocopに従いハッシュのキーと値が同じ場合は書かないようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     keys = %i[username postcode address biography]
-    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
-    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+    devise_parameter_sanitizer.permit(:sign_up, keys:)
+    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 
   def after_sign_in_path_for(_resource)
