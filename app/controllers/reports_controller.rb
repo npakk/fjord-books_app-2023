4e1a6d1d602dc3764c687333453befb806965e38
npakk <npakk669@gmail.com>
2025-06-07 23:59:03 +0900

    reportを更新削除できるのは本人のみにする

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :ensure_user, only: %i[ edit update destroy ]
 
   # GET /reports or /reports.json
   def index
@@ -67,5 +68,11 @@ class ReportsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def report_params
       params.require(:report).permit(:title, :body)
+    end
+
+    def ensure_user
+      if @report.user != current_user
+        redirect_to reports_path, alert: t('errors.messages.not_authorized')
+      end
     end
 end
