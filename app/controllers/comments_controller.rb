927eea8f1ba322244f40e60ceda2a12ce04ce987
npakk <npakk669@gmail.com>
2025-06-14 17:44:05 +0900

    コメントの日本語化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @commentable, notice: "Comment was successfully created." }
+        format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render_commentable_show }
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @commentable, notice: "Comment was successfully updated." }
+        format.html { redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human) }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -37,7 +37,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to @commentable, notice: "Comment was successfully destroyed." }
+      format.html { redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
       format.json { head :no_content }
     end
   end
