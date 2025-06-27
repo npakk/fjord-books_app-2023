10a4c124947eafe636548d79e642c2cf613073a3
npakk <npakk669@gmail.com>
2025-06-27 13:03:36 +0900

    日報の言及先と元をつなぐ中間テーブルを作成しバリデーションとユニーク制約をつける

---
+++ app/models/mention.rb
@@ -0,0 +1,6 @@
+class Mention < ApplicationRecord
+  belongs_to :mention, class_name: 'Report'
+  belongs_to :mentioned, class_name: 'Report'
+
+  validates :mention_id, uniqueness: { scope: :mentioned_id }
+end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,7 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :reports, through: :mentions, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
