8f75237ce4e1c6cec8d73e507c76d2dfacd885e0
npakk <npakk669@gmail.com>
2025-07-01 11:08:50 +0900

    その日報が言及している日報たち及びその日報に言及している日報たちを取得できるようにアソシエーションを設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,18 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :reports, through: :mentions, dependent: :destroy
+  has_many :mention_relationships,
+    class_name: 'Mention',
+    foreign_key: 'mention_id',
+    dependent: :destroy,
+    inverse_of: :mention
+  has_many :mentioning_reports, through: :mention_relationships, source: :mentioned
+  has_many :mentioned_relationships,
+    class_name: 'Mention',
+    foreign_key: 'mentioned_id',
+    dependent: :destroy,
+    inverse_of: :mentioned
+  has_many :mentioned_reports, through: :mentioned_relationships, source: :mention
 
   validates :title, presence: true
   validates :content, presence: true
